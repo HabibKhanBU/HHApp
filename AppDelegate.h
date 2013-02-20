de01adacd37767a9d6cea6a206db1285a7ed1cb6
vee0tv <veronica.tverbakk@gmail.com>
2013-02-19 22:53:15 -0500

    xCode MainTest

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MainMenuTest
+//
+//  Created by Tverbakk, Veronica, Kristine on 2/14/13.
+//  Copyright (c) 2013 Tverbakk, Veronica, Kristine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MainMenuTest
+//
+//  Created by Tverbakk, Veronica, Kristine on 2/14/13.
+//  Copyright (c) 2013 Tverbakk, Veronica, Kristine. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
